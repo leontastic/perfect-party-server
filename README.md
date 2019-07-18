6264d1a0b4a7662f61e45f7ba55dba499e4ceb18
Leon Li <la2li@uwaterloo.ca>
2019-07-18 01:38:50 -0400

    working REST endpoints and SQL queries

--- README.md
+++ README.md
@@ -2,6 +2,17 @@
 
 ## Prerequisites
 
-PostgreSQL 11
+### PostgreSQL 11
 
-Create a database called `perfect-party`.
+Install Postgres and create a database called `perfect-party`.
+
+### `.env` file
+
+Create a file named `.env` in the root directory to store environment variables:
+
+```
+PGUSER=homersimpson
+PGDATABASE=perfect-party
+```
+
+See `pg` docs to learn about environment variables it uses to connect to the database: https://node-postgres.com/features/connecting
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const Koa = require('koa')
 const cors = require('@koa/cors')
+const Router = require('koa-router')
+const db = require('./db/client')
 
 const app = new Koa()
+const router = new Router()
 
-app.use(cors())
+const getCurrentTime = async () => (await db.query('SELECT NOW()')).rows[0]
 
-app.use(async (ctx) => {
-  ctx.body = 'hello world'
+router.get('/current-time', async (ctx, next) => {
+  ctx.body = await getCurrentTime();
 })
 
+app.use(cors())
+app.use(router.routes())
 app.listen(8000)
